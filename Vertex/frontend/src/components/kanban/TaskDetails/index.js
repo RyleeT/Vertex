45921a975ce65dfc394356a2a29b027437fd9366
RyleeT <37136149+RyleeT@users.noreply.github.com>
2020-07-30 17:32:55 -0400

    Added task details skeleton for future update

---
+++ Vertex/frontend/src/components/kanban/TaskDetails/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// TODO: Add details for each task, priorities, etc
+
+// export class TaskDetails extends Component {
+//   render() {
+//     return (
+
+//     );
+//   }
+// }
+
+// export default TaskDetails;
