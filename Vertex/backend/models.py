ff66f105703aaae25ce14d6671cb41c214342190
RyleeT <37136149+RyleeT@users.noreply.github.com>
2020-07-30 17:46:11 -0400

    Removed uniqueness in Leads model

--- Vertex/backend/models.py
+++ Vertex/backend/models.py
@@ -5,7 +5,7 @@ from django.contrib.postgres.fields import ArrayField
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100)
     message = models.CharField(max_length=500, blank=True)
     owner = models.ForeignKey(
         User, related_name="leads", on_delete=models.CASCADE, null=True
