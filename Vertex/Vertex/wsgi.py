adb199e3255a425f6afc2007111ee108f6aca8d8
RyleeT <37136149+RyleeT@users.noreply.github.com>
2020-07-31 14:07:34 -0400

    Updated wsgi for deployment

--- Vertex/Vertex/wsgi.py
+++ Vertex/Vertex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vertex.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Vertex.Vertex.settings")
 
 application = get_wsgi_application()
