9bbab29046726c60429f5ff2dc7a99b27564b12d
RyleeT <37136149+RyleeT@users.noreply.github.com>
2020-08-10 00:37:17 -0400

    Configured webpack to load source maps

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,11 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
     ],
   },
 };
